16d9e8a6225683dc3add791adba124473bffe9d8
Leonlafrappe <leodunia8@gmail.com>
2025-03-17 13:05:37 -0400

    Add files via upload

---
+++ Lab2/js/hello.js
@@ -0,0 +1,19 @@
+var today = new Date();
+var hourNow = today.getHours();
+var minutesNow = today.getMinutes();
+var greeting;
+if (hourNow > 18) {
+ greeting = 'Good evening';
+}
+else if (hourNow > 12) {
+ greeting = 'Good afternoon';
+}
+else if (hourNow > 0) {
+ greeting = 'Good morning';
+}
+else {
+ greeting = 'Welcome';
+}
+ document.write(greeting);
+ document.write("<br>");
+ document.write("Current time is " + hourNow + ":" + minutesNow);
